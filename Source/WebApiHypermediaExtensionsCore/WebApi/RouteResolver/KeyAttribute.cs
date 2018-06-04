31e5c6379aada6e43643887f459793995c2f6351
Alexander Wiedemann <wiedemann@bluehands.de>
2018-06-04 10:44:33 +0200

    - attribute usage for KeyAttribute

--- Source/WebApiHypermediaExtensionsCore/WebApi/RouteResolver/KeyAttribute.cs
+++ Source/WebApiHypermediaExtensionsCore/WebApi/RouteResolver/KeyAttribute.cs
@@ -2,6 +2,12 @@ using System;
 
 namespace WebApiHypermediaExtensionsCore.WebApi.RouteResolver
 {
+    /// <summary>
+    /// Marks a property as (part of) the objects key. The route template to obtain the object 
+    /// must contain parameters matching the TemplateParameterName property. If a single property contains the complete
+    /// key information it is not neccessary and redundant to specify TemplateParameterName.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
     public class KeyAttribute : Attribute
     {
         public string TemplateParameterName { get; set; }
