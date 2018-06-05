38670fe4eaa754f248fc565742c061a72cd67736
Alexander Wiedemann <wiedemann@bluehands.de>
2018-06-05 10:21:08 +0200

    - fix TypeExtensionTests

--- Source/WebApi.HypermediaExtensions.Test/Hypermedia/TypeExtensionTest.cs
+++ Source/WebApi.HypermediaExtensions.Test/Hypermedia/TypeExtensionTest.cs
@@ -4,25 +4,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace WebApi.HypermediaExtensions.Test.Hypermedia
 {
-    
-
     [TestClass]
     public class TypeExtensionTest
     {
         [TestMethod]
-        [Ignore]
         public void GetGenericTypeName()
         {
             var typeName = typeof(Nullable<int>).BeautifulName();
-            Assert.AreEqual("Nullable<int>", typeName);
+            Assert.AreEqual("Nullable<Int32>", typeName);
         }
 
         [TestMethod]
-        [Ignore]
         public void GetNestedTypeName()
         {
             var typeName = typeof(Outer.Inner).BeautifulName();
-            Assert.AreEqual("Outer.Inner", typeName);
+            Assert.AreEqual($"{nameof(TypeExtensionTest)}.{nameof(Outer)}.{nameof(Outer.Inner)}", typeName);
         }
 
         class Outer
