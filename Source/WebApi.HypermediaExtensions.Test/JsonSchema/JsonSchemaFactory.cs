2dba8f0dfbd56432d1d5ba8effc77866886dd07a
Alexander Wiedemann <wiedemann@bluehands.de>
2018-06-06 17:21:15 +0200

    - asnyc schema factory

---
+++ Source/WebApi.HypermediaExtensions.Test/JsonSchema/JsonSchemaFactory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NJsonSchema;
+using NJsonSchema.Generation;
+using WebApi.HypermediaExtensions.Util;
+
+namespace WebApi.HypermediaExtensions.Test.JsonSchema
+{
+    public static class JsonSchemaFactory
+    {
+        static readonly JsonSchemaGeneratorSettings s_JsonSchemaGeneratorSettings = new JsonSchemaGeneratorSettings
+        {
+            FlattenInheritanceHierarchy = true,
+            DefaultEnumHandling = EnumHandling.String,
+
+        };
+
+        public static async Task<object> Generate(Type type)
+        {
+            var schema = await GenerateSchemaAsync(type).ConfigureAwait(false);
+            var schemaData = schema.ToJson();
+            return JsonConvert.DeserializeObject(schemaData);
+        }
+
+        public static async Task<JsonSchema4> GenerateSchemaAsync(Type type)
+        {
+            var schema = await JsonSchema4.FromTypeAsync(type, s_JsonSchemaGeneratorSettings).ConfigureAwait(false);
+            var keyProperties = type.GetTypeInfo().GetProperties()
+                .Select(p => new { p, att = p.GetCustomAttribute<KeyFromUriAttribute>() })
+                .Where(p => p.att != null)
+                .ToImmutableArray();
+
+            foreach (var keyProperty in keyProperties)
+            {
+                RemoveProperty(schema, keyProperty.p.Name);
+            }
+
+            foreach (var propertyGroup in keyProperties.GroupBy(p => p.att.SchemaProperyName ?? p.p.Name))
+            {
+                var schemaPropertyName = propertyGroup.Key;
+                if (schema.Properties.ContainsKey(schemaPropertyName))
+                {
+                    throw new JsonSchemaGenerationException($"Key property '{propertyGroup.First().p.Name}' maps to property '{schemaPropertyName}' that already exists on type {type.BeautifulName()}");
+                }
+
+                var property = new JsonProperty { Type = JsonObjectType.String, Format = JsonFormatStrings.Uri, MinLength = 1 };
+                AddRequiredProperty(schema, schemaPropertyName, property);
+            }
+
+            return schema;
+        }
+
+        static void RemoveProperty(JsonSchema4 schema, string propertyName)
+        {
+            schema.Properties.Remove(propertyName);
+            schema.RequiredProperties.Remove(propertyName);
+        }
+
+        static void AddRequiredProperty(JsonSchema4 schema, string propertyName, JsonProperty property)
+        {
+            schema.Properties.Add(propertyName, property);
+            schema.RequiredProperties.Add(propertyName);
+        }
+
+        public class JsonSchemaGenerationException : Exception
+        {
+            public JsonSchemaGenerationException(string message) : base(message)
+            {
+            }
+        }
+    }
+}
--- Source/WebApi.HypermediaExtensions.Test/JsonSchema/JsonSchemaGeneratorTest.cs
+++ Source/WebApi.HypermediaExtensions.Test/JsonSchema/JsonSchemaGeneratorTest.cs
@@ -1,28 +1,23 @@
 ﻿using System;
-using System.Collections.Immutable;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Reflection;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
 using NJsonSchema;
-using NJsonSchema.Generation;
 using WebApi.HypermediaExtensions.Hypermedia;
 using WebApi.HypermediaExtensions.Hypermedia.Actions;
 using WebApi.HypermediaExtensions.Test.Hypermedia;
-using WebApi.HypermediaExtensions.Util;
 
 namespace WebApi.HypermediaExtensions.Test.JsonSchema
 {
     [TestClass]
-    public class When_generating_json_schema_from_type_with_key_attribute : TestSpecification
+    public class When_generating_json_schema_from_type_with_key_attribute : AsyncTestSpecification
     {
         JsonSchema4 schema;
 
-        public override void When()
+        protected override async Task When()
         {
-            schema = JsonSchemaFactory.GenerateSchema(typeof(MyParameter));
+            schema = await JsonSchemaFactory.GenerateSchemaAsync(typeof(MyParameter)).ConfigureAwait(false);
         }
 
         [TestMethod]
@@ -31,32 +26,31 @@ namespace WebApi.HypermediaExtensions.Test.JsonSchema
             schema.RequiredUriPropertyShouldExist("Id");
         }
 
-        public class MyParameter : IHypermediaActionParameter
+        class MyParameter : IHypermediaActionParameter
         {
+            // ReSharper disable UnusedMember.Local
             [Required]
-            [KeyFromRoute(typeof(MyHypermediaObject))]
+            [KeyFromUri(typeof(MyHypermediaObject))]
             public Guid Id { get; set; }
-
             public int SomeValue { get; set; }
-
             [Required]
             public Uri Uri { get; set; }
+            // ReSharper restore UnusedMember.Local
         }
 
-        public class MyHypermediaObject : HypermediaObject
+        class MyHypermediaObject : HypermediaObject
         {
-
         }
     }
 
     [TestClass]
-    public class When_generating_json_schema_from_type_with_multiple_key_attributes : TestSpecification
+    public class When_generating_json_schema_from_type_with_multiple_key_attributes : AsyncTestSpecification
     {
         JsonSchema4 schema;
 
-        public override void When()
+        protected override async Task When()
         {
-            schema = JsonSchemaFactory.GenerateSchema(typeof(MyParameter));
+            schema = await JsonSchemaFactory.GenerateSchemaAsync(typeof(MyParameter)).ConfigureAwait(false);
         }
 
         [TestMethod]
@@ -67,33 +61,29 @@ namespace WebApi.HypermediaExtensions.Test.JsonSchema
             schema.Properties.Should().NotContainKeys("Id", "ParentId", "AnotherReferencedId", "AnotherReferencedIdParentId");
         }
 
-        public class MyParameter : IHypermediaActionParameter
+        class MyParameter : IHypermediaActionParameter
         {
+            // ReSharper disable UnusedMember.Local
             [Required]
-            [KeyFromRoute(typeof(MyHypermediaObject), "UriToHmo", "id")]
+            [KeyFromUri(typeof(MyHypermediaObject), "UriToHmo", "id")]
             public Guid Id { get; set; }
-
             [Required]
-            [KeyFromRoute(typeof(MyHypermediaObject), "UriToHmo", "parentId")]
+            [KeyFromUri(typeof(MyHypermediaObject), "UriToHmo", "parentId")]
             public Guid ParentId { get; set; }
-
             [Required]
-            [KeyFromRoute(typeof(MyHypermediaObject), "UriToAnotherHmo", "id")]
+            [KeyFromUri(typeof(MyHypermediaObject), "UriToAnotherHmo", "id")]
             public Guid AnotherReferencedId { get; set; }
-
             [Required]
-            [KeyFromRoute(typeof(MyHypermediaObject), "UriToAnotherHmo", "parentId")]
+            [KeyFromUri(typeof(MyHypermediaObject), "UriToAnotherHmo", "parentId")]
             public Guid AnotherReferencedIdParentId { get; set; }
-
             public int SomeValue { get; set; }
-
             [Required]
             public Uri Uri { get; set; }
+            // ReSharper restore UnusedMember.Local
         }
 
-        public class MyHypermediaObject : HypermediaObject
+        class MyHypermediaObject : HypermediaObject
         {
-
         }
     }
 
@@ -109,79 +99,22 @@ namespace WebApi.HypermediaExtensions.Test.JsonSchema
         }
     }
 
-    public class KeyFromRouteAttribute : Attribute
+    public class KeyFromUriAttribute : Attribute
     {
         public string SchemaProperyName { get; }
         public string RouteTemplateParameterName { get; }
         public Type ReferencedHypermediaObjectType { get; }
 
-        public KeyFromRouteAttribute(Type referencedHypermediaObjectType)
+        public KeyFromUriAttribute(Type referencedHypermediaObjectType)
         {
             ReferencedHypermediaObjectType = referencedHypermediaObjectType;
         }
 
-        public KeyFromRouteAttribute(Type referencedHypermediaObjectType, string schemaProperyName,
+        public KeyFromUriAttribute(Type referencedHypermediaObjectType, string schemaProperyName,
             string routeTemplateParameterName) : this(referencedHypermediaObjectType)
         {
             SchemaProperyName = schemaProperyName;
             RouteTemplateParameterName = routeTemplateParameterName;
-        }
-    }
-
-    public static class JsonSchemaFactory
-    {
-        static readonly JsonSchemaGeneratorSettings s_JsonSchemaGeneratorSettings = new JsonSchemaGeneratorSettings
-        {
-            FlattenInheritanceHierarchy = true,
-            DefaultEnumHandling = EnumHandling.String,
-
-        };
-
-        public static object Generate(Type type)
-        {
-            var schema = GenerateSchema(type);
-
-            var schemaData = schema.ToJson();
-            return JsonConvert.DeserializeObject(schemaData);
-        }
-
-        public static JsonSchema4 GenerateSchema(Type type)
-        {
-            var schema = JsonSchema4.FromTypeAsync(type, s_JsonSchemaGeneratorSettings).GetAwaiter().GetResult();
-            var keyProperties = type.GetTypeInfo().GetProperties()
-                .Select(p => new { p, att = p.GetCustomAttribute<KeyFromRouteAttribute>() })
-                .Where(p => p.att != null)
-                .ToImmutableArray();
-
-            if (keyProperties.Any())
-            {
-                foreach (var keyProperty in keyProperties)
-                {
-                    schema.Properties.Remove(keyProperty.p.Name);
-                    schema.RequiredProperties.Remove(keyProperty.p.Name);
-                }
-
-                var propertiesBySchemaPropertyName = keyProperties.GroupBy(p => p.att.SchemaProperyName ?? p.p.Name);
-                foreach (var propertyGroup in propertiesBySchemaPropertyName)
-                {
-                    var schemaPropertyName = propertyGroup.Key;
-                    if (schema.Properties.ContainsKey(schemaPropertyName))
-                    {
-                        throw new JsonSchemaGenerationException($"Key property '{propertyGroup.First().p.Name}' maps to property '{schemaPropertyName}' that already exists on type {type.BeautifulName()}");
-                    }
-                    schema.Properties.Add(schemaPropertyName, new JsonProperty { Type = JsonObjectType.String, Format = JsonFormatStrings.Uri, MinLength = 1 });
-                    schema.RequiredProperties.Add(schemaPropertyName);
-                }
-            }
-
-            return schema;
-        }
-
-        public class JsonSchemaGenerationException : Exception
-        {
-            public JsonSchemaGenerationException(string message) : base(message)
-            {
-            }
         }
     }
 }
