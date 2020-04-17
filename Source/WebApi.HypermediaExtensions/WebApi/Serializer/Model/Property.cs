7eea2282632b17bdba45217701e08791d3c7ea43
Tyrrx <david.retzlaff@bluehands.de>
2020-04-17 18:07:41 +0200

    Add Expressions to Property model

--- Source/WebApi.HypermediaExtensions/WebApi/Serializer/Model/Property.cs
+++ Source/WebApi.HypermediaExtensions/WebApi/Serializer/Model/Property.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using FunicularSwitch;
+using WebApi.HypermediaExtensions.Util.Extensions;
 
 namespace WebApi.HypermediaExtensions.WebApi.Serializer.Model
 {
@@ -9,15 +12,25 @@ namespace WebApi.HypermediaExtensions.WebApi.Serializer.Model
         {
             PropertyInfo = propertyInfo;
             SerializationName = propertyInfo.Name;
+            GetPropertyValue = propertyInfo.GetValueGetter<object>();
         }
         public Property(PropertyInfo propertyInfo, string serializationName)
         {
             PropertyInfo = propertyInfo;
             SerializationName = serializationName;
+            GetPropertyValue = propertyInfo.GetValueGetter<object>();
         }
+
+        protected Func<object, object> GetPropertyValue { get; }
 
         public string SerializationName { get; private set; }
         public PropertyInfo PropertyInfo { get; private set; }
+
+        public Result<object> GetValue(object hypermediaObject)
+        {
+           var value = GetPropertyValue.Invoke(hypermediaObject);
+           return value != null ? Result.Ok(value) : Result.Error<object>("");
+        }
 
     }
 }
