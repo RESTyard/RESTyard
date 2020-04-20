d32cc04540c9efcbe612841bcee71e125ab7a612
Mathias Reichardt <mathias.reichardt@posteo.de>
2020-04-20 16:05:30 +0200

    Add missinginterface

---
+++ Source/WebApi.HypermediaExtensions/WebApi/Serializer/Reflection/IObjectReflectionService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using FunicularSwitch;
+
+namespace WebApi.HypermediaExtensions.WebApi.Serializer.Reflection
+{
+    public interface IObjectReflectionService
+    {
+        Result<ObjectReflection> Reflect(Type type);
+    }
+}
