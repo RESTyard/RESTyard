7c1b0b35c121c28a37929d409ee3483ea44a68e8
Mathias Reichardt <mathias.reichardt@posteo.de>
2020-04-20 14:53:04 +0200

    Add propertiy which indicates if a self link should be generated by default for a HTO

--- Source/WebApi.HypermediaExtensions/Hypermedia/Attributes/HypermediaObjectAttribute.cs
+++ Source/WebApi.HypermediaExtensions/Hypermedia/Attributes/HypermediaObjectAttribute.cs
@@ -5,7 +5,19 @@ namespace WebApi.HypermediaExtensions.Hypermedia.Attributes
     [AttributeUsage(AttributeTargets.Class)]
     public sealed class HypermediaObjectAttribute : Primary
     {
+        /// <summary>
+        /// A title, describing the object for a Human
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Classes of this object, specifies the layout of this object
+        /// </summary>
         public string[] Classes { get; set; }
+
+        /// <summary>
+        /// If true no default self link should be generated for this object
+        /// </summary>
+        public bool NoDefaultSelfLink { get; set; }
     }
 }
