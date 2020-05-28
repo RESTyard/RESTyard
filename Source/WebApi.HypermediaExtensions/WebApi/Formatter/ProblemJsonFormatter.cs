537d62cc894b1e85d0df2b62cff3082aebd5f712
Mathias Reichardt <mathias.reichardt@posteo.de>
2020-05-28 07:43:41 +0200

    add multi framework targeting

--- Source/WebApi.HypermediaExtensions/WebApi/Formatter/ProblemJsonFormatter.cs
+++ Source/WebApi.HypermediaExtensions/WebApi/Formatter/ProblemJsonFormatter.cs
@@ -1,10 +1,12 @@
-﻿using System.Buffers;
-using Bluehands.Hypermedia.MediaTypes;
-using Microsoft.AspNetCore.Mvc.Formatters;
-using Newtonsoft.Json;
-
-namespace WebApi.HypermediaExtensions.WebApi.Formatter
+﻿namespace WebApi.HypermediaExtensions.WebApi.Formatter
 {
+    using Bluehands.Hypermedia.MediaTypes;
+    
+#if NETSTANDARD1_6
+    using System.Buffers;
+    using Newtonsoft.Json;
+    using Microsoft.AspNetCore.Mvc.Formatters;
+    
     public class ProblemJsonFormatter : JsonOutputFormatter
     {
         public ProblemJsonFormatter() : base(new JsonSerializerSettings(), ArrayPool<char>.Shared)
@@ -13,4 +15,19 @@ namespace WebApi.HypermediaExtensions.WebApi.Formatter
             SupportedMediaTypes.Add(DefaultMediaTypes.ProblemJson);
         }
     }
+
+#else
+
+    using System.Text.Json;
+    using Microsoft.AspNetCore.Mvc.Formatters;
+    public class ProblemJsonFormatter : SystemTextJsonOutputFormatter
+    {
+        public ProblemJsonFormatter() : base(new JsonSerializerOptions())
+        {
+            SupportedMediaTypes.Clear();
+            SupportedMediaTypes.Add(DefaultMediaTypes.ProblemJson);
+        }
+    }
+#endif
+
 }
