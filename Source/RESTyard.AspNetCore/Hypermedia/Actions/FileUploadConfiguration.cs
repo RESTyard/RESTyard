16643291810915be396f481e3272710efb86b735
Mathias Reichardt <reichardt@data-cybernetics.com>
2023-04-12 16:22:38 +0200

    Change configuration so it matches html input file specification

--- Source/RESTyard.AspNetCore/Hypermedia/Actions/FileUploadConfiguration.cs
+++ Source/RESTyard.AspNetCore/Hypermedia/Actions/FileUploadConfiguration.cs
@@ -5,18 +5,25 @@ namespace RESTyard.AspNetCore.Hypermedia.Actions;
 public record FileUploadConfiguration
 {
     /// <summary>
-    /// Max size of a single file in bytes the server will accept
+    /// Max size of a single file in bytes the server will accept.
+    /// -1 indicating no limit.
     /// </summary>
     public long MaxFileSizeBytes { get; set; } = -1;
     
     /// <summary>
-    /// Max number of files accepted for upload. Default is 1 
+    /// Indicates if it is allowed to send multiple files.
     /// </summary>
-    public int MaxFileCount { get; set; } = 1;
+    public bool AllowMultiple { get; set; } = false;
     
     /// <summary>
-    /// List of extensions which the client can use to limit file selection. Must not contain '.'
-    /// Default is empty indicating no limit. 
+    /// Defines the file types the file input should accept as unique file type specifiers.
+    /// Examples:
+    /// <list type="bullet">
+    /// <item><description>case-insensitive filename extension, starting with a period ("."). E.g.: '.png'</description></item>
+    /// <item><description>A valid MIME type string, with no extensions. E.g.: 'text/html'</description></item>
+    /// <item><description>Media wildcard. E.g.: 'image/*', 'audio/*', 'video/*'</description></item>
+    /// </list>
+    /// Default is empty indicating no limitation.
     /// </summary>
-    public List<string> AllowedFileExtensions { get; set; } = new();
+    public List<string> Accepted { get; set; } = new();
 }
