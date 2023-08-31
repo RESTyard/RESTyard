8f8676c292fc9dcab92d024932d9edbe05061398
Jasper Park <jasper.park@bluehands.de>
2023-08-31 16:26:37 +0200

    feat: Downgrade client project to netstandard2.0 because it can be (with langversion=latest) which enables older projects to use the most recent version still

---
+++ Source/RESTyard.Client.Extensions/NewtonsoftJson/EnableLanguageFeatures.cs
@@ -0,0 +1,29 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    internal class IsExternalInit
+    {
+    
+    }
+}
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class NotNullWhenAttribute : Attribute
+    {
+        public NotNullWhenAttribute(bool returnValue)
+        {
+            ReturnValue = returnValue;
+        }
+
+        public bool ReturnValue { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
+    public sealed class NotNullIfNotNullAttribute : Attribute
+    {
+        public NotNullIfNotNullAttribute(string parameterName) => ParameterName = parameterName;
+
+        public string ParameterName { get; }
+    }
+}
---
+++ Source/RESTyard.Client/Extensions/EnableLanguageFeatures.cs
@@ -0,0 +1,21 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    internal class IsExternalInit
+    {
+    
+    }
+}
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class NotNullWhenAttribute : Attribute
+    {
+        public NotNullWhenAttribute(bool returnValue)
+        {
+            ReturnValue = returnValue;
+        }
+
+        public bool ReturnValue { get; }
+    }
+}
