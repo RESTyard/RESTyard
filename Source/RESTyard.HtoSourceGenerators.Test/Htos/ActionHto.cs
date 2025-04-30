68e7b3c983ee433b5e0a3088f54d2558405526b8
Mathias Reichardt <reichardt@data-cybernetics.com>
2025-04-30 09:41:15 +0200

    Add comments

--- Source/RESTyard.HtoSourceGenerators.Test/Htos/ActionHto.cs
+++ Source/RESTyard.HtoSourceGenerators.Test/Htos/ActionHto.cs
@@ -17,12 +17,14 @@ public class ActionHto
     // dynamic action
     // file upload
     // specify default in assignment
+    // add [Mandatory] to indicate this action is always available independent of state and authorization
 
     // in controllers:
     // could work like this: [HypermediaAction(HtoAction = nameof(ActionHto.ReCalculateOp), HtoClass = nameof(ActionHto))]
     // -> type save, navigatable, refactor save
     // -> works for parameterless actions
     // -> for parameter actions not needed since we coudl match by parameter name, BUT enables parameter reuse! 
+    // how to make actiosn that produce a link indicate the result link type e.g. generate Car query will return a CarQueryResultHto link.
     
     // for get routes:
     // [HypermediaAccessTo<ActionHto>]??, better use [ProducesResponseType(...)] / ActionResult<Hto> and modify ApiExplorer so OpenApi is correct
