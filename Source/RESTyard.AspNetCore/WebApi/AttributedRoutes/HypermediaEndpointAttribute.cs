67f097bf2090a6780c06fa5f1bfc46810c1a4a06
Jasper Park <jasper.park@bluehands.de>
2025-05-16 23:09:47 +0200

    feat: introduce new attributes and implement AttributedRoutesRegister.cs code to handle the new attributes

---
+++ Source/RESTyard.AspNetCore/WebApi/AttributedRoutes/HypermediaEndpointAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using FunicularSwitch.Extensions;
+using Microsoft.AspNetCore.Routing;
+using RESTyard.AspNetCore.Exceptions;
+using RESTyard.AspNetCore.Hypermedia.Attributes;
+
+namespace RESTyard.AspNetCore.WebApi.AttributedRoutes;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+public class HypermediaEndpointAttribute : Attribute, IEndpointNameMetadata
+{
+    public Type RouteType { get; }
+    public Type? RouteKeyProducerType { get; }
+        
+    public string? AcceptedMediaType { get; }
+    
+    public string EndpointName { get; }
+
+    
+    public Type? ActionType { get; }
+
+    public HypermediaEndpointAttribute(Type routeType, Type? routeKeyProducerType = null)
+    {
+        this.RouteType = routeType;
+        this.RouteKeyProducerType = routeKeyProducerType;
+        AttributedRouteHelper.EnsureHas<HypermediaObjectAttribute>(this.RouteType);
+        AttributedRouteHelper.EnsureIsRouteKeyProducer(routeKeyProducerType);
+        this.EndpointName = AttributedRouteHelper.EscapeRouteName($"GenericRouteName_HypermediaObject_{this.RouteType}");
+    }
+
+    public HypermediaEndpointAttribute(Type routeType, string actionPropertyName, string? acceptedMediaType = null) : this(routeType)
+    {
+        this.AcceptedMediaType = acceptedMediaType;
+        var property = this.RouteType.GetProperty(actionPropertyName, BindingFlags.Public | BindingFlags.Instance);
+        if (property is null)
+        {
+            throw new HypermediaException(
+                $"Property '{actionPropertyName}' not found on type {this.RouteType.BeautifulName()}");
+        }
+
+        this.ActionType = property.PropertyType;
+        this.EndpointName = AttributedRouteHelper.EscapeRouteName($"GenericRouteName_HypermediaAction_{this.ActionType}");
+    }
+}
--- Source/RESTyard.AspNetCore/WebApi/RouteResolver/AttributedRoutesRegister.cs
+++ Source/RESTyard.AspNetCore/WebApi/RouteResolver/AttributedRoutesRegister.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Routing.Template;
 using Microsoft.Extensions.Logging;
 using RESTyard.AspNetCore.Exceptions;
 using RESTyard.AspNetCore.Hypermedia;
+using RESTyard.AspNetCore.Util;
 using RESTyard.AspNetCore.Util.Extensions;
 using RESTyard.AspNetCore.WebApi.AttributedRoutes;
 using RESTyard.AspNetCore.WebApi.ExtensionMethods;
@@ -36,41 +37,85 @@ namespace RESTyard.AspNetCore.WebApi.RouteResolver
             var assemblyMethods = assembly.GetTypes().SelectMany(t => t.GetTypeInfo().GetMethods());
             foreach (var method in assemblyMethods)
             {
-                if (!TryGetSingleHypermediaAttribute(method, out var hypermediaAttribute))
+                if (TryGetHypermediaEndpoint<HypermediaEndpointAttribute>(method, out var endpointAttribute, out var httpAttribute))
                 {
-                    continue;
-                }
-                
-                AssertAttributeHasName(hypermediaAttribute);
+                    AssertAttributeHasName(endpointAttribute);
 
-                switch (hypermediaAttribute)
+                    switch (httpAttribute)
+                    {
+                        case HttpGetAttribute:
+                            this.AddHypermediaObjectRoute(endpointAttribute.RouteType, endpointAttribute.EndpointName, HttpMethod.GET);
+                            break;
+                        case HttpPostAttribute:
+                            this.AddActionRoute(endpointAttribute.ActionType!, endpointAttribute.EndpointName, HttpMethod.POST, endpointAttribute.AcceptedMediaType);
+                            break;
+                        case HttpPutAttribute:
+                            this.AddActionRoute(endpointAttribute.ActionType!, endpointAttribute.EndpointName, HttpMethod.PUT, endpointAttribute.AcceptedMediaType);
+                            break;
+                        case HttpPatchAttribute:
+                            this.AddActionRoute(endpointAttribute.ActionType!, endpointAttribute.EndpointName, HttpMethod.PATCH, endpointAttribute.AcceptedMediaType);
+                            break;
+                        case HttpDeleteAttribute:
+                            this.AddActionRoute(endpointAttribute.ActionType!, endpointAttribute.EndpointName, HttpMethod.DELETE, endpointAttribute.AcceptedMediaType);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(httpAttribute),
+                                $"Unknown http attribute: {httpAttribute.GetType().Name}");
+                    }
+                    this.AddRouteKeyProducer(method, endpointAttribute, httpAttribute);
+                }
+                else if (TryGetHypermediaEndpoint<HypermediaParameterInfoAttribute>(method, out var parameterInfoAttribute, out httpAttribute))
                 {
-                    case HttpGetHypermediaObject httpGetHypermediaObject:
-                        this.AddHypermediaObjectRoute(httpGetHypermediaObject.RouteType, httpGetHypermediaObject.Name!, HttpMethod.GET);
-                        AddRouteKeyProducer(method, httpGetHypermediaObject);
-                        break;
-                    case HttpPostHypermediaAction httpPostHypermediaAction:
-                        this.AddActionRoute(httpPostHypermediaAction.RouteType, httpPostHypermediaAction.Name!, HttpMethod.POST, httpPostHypermediaAction.AcceptedMediaType);
-                        AddRouteKeyProducer(method, httpPostHypermediaAction);
-                        break;
-                    case HttpPutHypermediaAction httpPutHypermediaAction:
-                        this.AddActionRoute(httpPutHypermediaAction.RouteType, httpPutHypermediaAction.Name!, HttpMethod.PUT, httpPutHypermediaAction.AcceptedMediaType);
-                        AddRouteKeyProducer(method, httpPutHypermediaAction);
-                        break;
-                    case HttpDeleteHypermediaAction httpDeleteHypermediaAction:
-                        this.AddActionRoute(httpDeleteHypermediaAction.RouteType, httpDeleteHypermediaAction.Name!, HttpMethod.DELETE, httpDeleteHypermediaAction.AcceptedMediaType);
-                        AddRouteKeyProducer(method, httpDeleteHypermediaAction);
-                        break;
-                    case HttpPatchHypermediaAction httpPatchHypermediaAction:
-                        this.AddActionRoute(httpPatchHypermediaAction.RouteType, httpPatchHypermediaAction.Name!, HttpMethod.PATCH, httpPatchHypermediaAction.AcceptedMediaType);
-                        AddRouteKeyProducer(method, httpPatchHypermediaAction);
-                        break;
-                    case HttpGetHypermediaActionParameterInfo httpGetHypermediaActionParameterInfo:
-                        this.AddParameterTypeRoute(httpGetHypermediaActionParameterInfo.RouteType, httpGetHypermediaActionParameterInfo.Name!, HttpMethod.GET);
-                        AddRouteKeyProducer(method, httpGetHypermediaActionParameterInfo);
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(hypermediaAttribute), $"Unknown hypermedia attribute: {hypermediaAttribute.GetType().Name}");
+                    if (httpAttribute is HttpGetAttribute httpGetAttribute)
+                    {
+                        this.AddParameterTypeRoute(parameterInfoAttribute.RouteType, parameterInfoAttribute.EndpointName, HttpMethod.GET);
+                    }
+                    else
+                    {
+                        throw new HypermediaException(
+                            $"Unsupported HTTP verb {httpAttribute.GetType().BeautifulName()} on parameter info endpoint");
+                    }
+                }
+                else if (TryGetSingleHypermediaAttribute(method, out var hypermediaAttribute))
+                {
+                    AssertAttributeHasName(hypermediaAttribute);
+
+                    switch (hypermediaAttribute)
+                    {
+                        case HttpGetHypermediaObject httpGetHypermediaObject:
+                            this.AddHypermediaObjectRoute(httpGetHypermediaObject.RouteType,
+                                httpGetHypermediaObject.Name!, HttpMethod.GET);
+                            AddRouteKeyProducer(method, httpGetHypermediaObject);
+                            break;
+                        case HttpPostHypermediaAction httpPostHypermediaAction:
+                            this.AddActionRoute(httpPostHypermediaAction.RouteType, httpPostHypermediaAction.Name!,
+                                HttpMethod.POST, httpPostHypermediaAction.AcceptedMediaType);
+                            AddRouteKeyProducer(method, httpPostHypermediaAction);
+                            break;
+                        case HttpPutHypermediaAction httpPutHypermediaAction:
+                            this.AddActionRoute(httpPutHypermediaAction.RouteType, httpPutHypermediaAction.Name!,
+                                HttpMethod.PUT, httpPutHypermediaAction.AcceptedMediaType);
+                            AddRouteKeyProducer(method, httpPutHypermediaAction);
+                            break;
+                        case HttpDeleteHypermediaAction httpDeleteHypermediaAction:
+                            this.AddActionRoute(httpDeleteHypermediaAction.RouteType, httpDeleteHypermediaAction.Name!,
+                                HttpMethod.DELETE, httpDeleteHypermediaAction.AcceptedMediaType);
+                            AddRouteKeyProducer(method, httpDeleteHypermediaAction);
+                            break;
+                        case HttpPatchHypermediaAction httpPatchHypermediaAction:
+                            this.AddActionRoute(httpPatchHypermediaAction.RouteType, httpPatchHypermediaAction.Name!,
+                                HttpMethod.PATCH, httpPatchHypermediaAction.AcceptedMediaType);
+                            AddRouteKeyProducer(method, httpPatchHypermediaAction);
+                            break;
+                        case HttpGetHypermediaActionParameterInfo httpGetHypermediaActionParameterInfo:
+                            this.AddParameterTypeRoute(httpGetHypermediaActionParameterInfo.RouteType,
+                                httpGetHypermediaActionParameterInfo.Name!, HttpMethod.GET);
+                            AddRouteKeyProducer(method, httpGetHypermediaActionParameterInfo);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(hypermediaAttribute),
+                                $"Unknown hypermedia attribute: {hypermediaAttribute.GetType().Name}");
+                    }
                 }
             }
         }
@@ -78,7 +123,7 @@ namespace RESTyard.AspNetCore.WebApi.RouteResolver
         private void AddRouteKeyProducer<T>(MethodInfo method, T hypermediaAttribute)
             where T : HttpMethodAttribute, IHaveRouteInfo
         {
-            var autoAddRouteKeyProducers = hypermediaAttribute is HttpGetHypermediaObject;
+            var autoAddRouteKeyProducers = method.GetCustomAttribute<HttpGetAttribute>(true) is not null;
           
             if (hypermediaAttribute.RouteKeyProducerType != null)
             {
@@ -107,6 +152,37 @@ namespace RESTyard.AspNetCore.WebApi.RouteResolver
             }
         }
 
+        private void AddRouteKeyProducer(MethodInfo method, HypermediaEndpointAttribute hypermediaAttribute, HttpMethodAttribute httpAttribute)
+        {
+            var autoAddRouteKeyProducers = httpAttribute is HttpGetAttribute;
+          
+            if (hypermediaAttribute.RouteKeyProducerType != null)
+            {
+                if (typeof(HypermediaQueryResult).GetTypeInfo().IsAssignableFrom(hypermediaAttribute.RouteType))
+                {
+                    throw new RouteRegisterException(
+                        $"Routes to Query's may not require a key '{hypermediaAttribute.RouteType}'. Queries should not be handled on a Entity.");
+                }
+
+                AttributedRouteHelper.AssertIsRouteKeyProducer(hypermediaAttribute.RouteKeyProducerType);
+                var keyProducer = (IKeyProducer)Activator.CreateInstance(hypermediaAttribute.RouteKeyProducerType)!;
+                this.AddRouteKeyProducer(hypermediaAttribute.RouteType, keyProducer);
+            }
+            else if (autoAddRouteKeyProducers && !typeof(HypermediaQueryResult).GetTypeInfo().IsAssignableFrom(hypermediaAttribute.RouteType))
+            {
+                var templateToUse = httpAttribute.Template ?? string.Empty;
+                var controllerRouteSegments = GetControllerRouteSegment(method);
+
+                var template = TemplateParser.Parse(controllerRouteSegments + templateToUse);
+                if (template.Parameters.Count > 0)
+                {
+                    this.AddRouteKeyProducer(
+                        hypermediaAttribute.RouteType,
+                        RouteKeyProducer.Create(hypermediaAttribute.RouteType, template.Parameters.Select(p => p.Name).ToList()));
+                }
+            }
+        }
+
         private static void AssertAttributeHasName(HttpMethodAttribute hypermediaAttribute)
         {
             if (string.IsNullOrEmpty(hypermediaAttribute.Name))
@@ -115,10 +191,52 @@ namespace RESTyard.AspNetCore.WebApi.RouteResolver
             }
         }
 
+        private static void AssertAttributeHasName(HypermediaEndpointAttribute hypermediaAttribute)
+        {
+            if (string.IsNullOrEmpty(hypermediaAttribute.EndpointName))
+            {
+                throw new RouteRegisterException($"{typeof(HypermediaEndpointAttribute).BeautifulName()} must have a name.");
+            }
+        }
+
+        private static bool TryGetHypermediaEndpoint<T>(
+            MethodInfo method,
+            [NotNullWhen(true)] out T? hypermediaEndpointAttribute,
+            [NotNullWhen(true)] out HttpMethodAttribute? httpAttribute)
+            where T : Attribute
+        {
+            var httpAttributes = method.GetCustomAttributes<HttpMethodAttribute>(true).ToList();
+            var hypermediaEndpoints = method.GetCustomAttributes<T>(true).ToList();
+
+            if (httpAttributes is [] || hypermediaEndpoints is [])
+            {
+                hypermediaEndpointAttribute = null;
+                httpAttribute = null;
+                return false;
+            }
+
+            if (httpAttributes is { Count: > 1 })
+            {
+                throw new RouteRegisterException(
+                    $"More than one {typeof(HttpMethodAttribute).BeautifulName()} on hypermedia endpoint {method.Name} is not supported.");
+            }
+
+            if (hypermediaEndpoints is { Count: > 1 })
+            {
+                throw new RouteRegisterException(
+                    $"More than one {typeof(T).BeautifulName()} on endpoint {method.Name}");
+            }
+
+
+            hypermediaEndpointAttribute = hypermediaEndpoints[0];
+            httpAttribute = httpAttributes[0];
+            return true;
+        }
+
         private static bool TryGetSingleHypermediaAttribute(MethodInfo method, [NotNullWhen(true)] out HttpMethodAttribute? hypermediaAttribute)
         {
             var httpMethodAttributes = method.GetCustomAttributes<HttpMethodAttribute>(true)
-                .Where(IsExtensionAttribute)
+                .Where(IsLegacyExtensionAttribute)
                 .ToArray();
             if (httpMethodAttributes.Length == 0)
             {
@@ -135,15 +253,10 @@ namespace RESTyard.AspNetCore.WebApi.RouteResolver
             return true;
         }
         
-        private static bool IsExtensionAttribute(HttpMethodAttribute a)
+        private static bool IsLegacyExtensionAttribute(HttpMethodAttribute a)
         {
-            var attributeType = a.GetType();
-            return attributeType == typeof(HttpGetHypermediaObject)
-                   || attributeType == typeof(HttpPutHypermediaAction)
-                   || attributeType == typeof(HttpPostHypermediaAction)
-                   || attributeType == typeof(HttpDeleteHypermediaAction)
-                   || attributeType == typeof(HttpPatchHypermediaAction)
-                   || attributeType == typeof(HttpGetHypermediaActionParameterInfo);
+            return a is HttpGetHypermediaObject or HttpPostHypermediaAction or HttpPatchHypermediaAction
+                or HttpPutHypermediaAction or HttpDeleteHypermediaAction or HttpGetHypermediaActionParameterInfo;
         }
 
         private string GetControllerRouteSegment(MethodInfo method)
