b67acd337aae971b2fead5aef42c27da5b47f72b
David Retzlaff <david.retzlaff@bluehands.de>
2025-08-12 11:13:08 +0200

    fix: move pagination extensions to server side package as they should not be used in the client

--- Source/RESTyard.AspNetCore.Extensions.Pagination/PageClippingExtensions.cs
+++ Source/RESTyard.AspNetCore.Extensions.Pagination/PageClippingExtensions.cs
@@ -1,13 +1,13 @@
-namespace RESTyard.Extensions.Pagination;
+namespace RESTyard.AspNetCore.Extensions.Pagination;
 
 public static class PageClippingExtensions
 {
-    public static IEnumerable<T> ClipToPagination<T>(this IEnumerable<T> source, Pagination pagination) =>
+    public static IEnumerable<T> ClipToPagination<T>(this IEnumerable<T> source, RESTyard.Extensions.Pagination.Pagination pagination) =>
         pagination.IsDisabled
             ? source
             : source.Skip(pagination.PageSize * pagination.PageOffset).Take(pagination.PageSize);
     
-    public static IQueryable<T> ClipToPagination<T>(this IQueryable<T> source, Pagination pagination) =>
+    public static IQueryable<T> ClipToPagination<T>(this IQueryable<T> source, RESTyard.Extensions.Pagination.Pagination pagination) =>
         pagination.IsDisabled
             ? source
             : source.Skip(pagination.PageSize * pagination.PageOffset).Take(pagination.PageSize);
