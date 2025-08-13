40b2274c5de2aa3bfe20e2918ce1cce23a75d17e
David Retzlaff <david.retzlaff@bluehands.de>
2025-08-13 16:40:16 +0200

    fix: change properties to read-only

--- Source/RESTyard.AspNetCore.Extensions.Pagination/IQueryResult.cs
+++ Source/RESTyard.AspNetCore.Extensions.Pagination/IQueryResult.cs
@@ -4,15 +4,15 @@ namespace RESTyard.AspNetCore.Extensions.Pagination;
 /// Represents the result of a query operation that includes a collection of entities and their total count that match the query criteria, regardless of pagination.
 /// </summary>
 /// <typeparam name="TEntity">The type of the entity</typeparam>
-public interface IQueryResult<TEntity>
+public interface IQueryResult<out TEntity>
 {
     /// <summary>
     /// Represents the collection of entities returned by a query.
     /// </summary>
-    IEnumerable<TEntity> Entities { get; set; }
+    IEnumerable<TEntity> Entities { get; }
 
     /// <summary>
     /// Represents the total count of entities that match the query criteria, regardless of pagination.
     /// </summary>
-    int TotalCountOfEntities { get; set; }
+    int TotalCountOfEntities { get; }
 }
