ccd3f362be8463c37dac125a962b6ce6987054fb
Jasper Park <jasper.park@bluehands.de>
2025-11-10 15:58:45 +0100

    feat: add Intellisense comment for new method and simplify by extending IApplicationBuilder instead of a generic TApplicationBuilder

--- Source/RESTyard.AspNetCore.HypermediaUI/HypermediaUiExtensions.cs
+++ Source/RESTyard.AspNetCore.HypermediaUI/HypermediaUiExtensions.cs
@@ -11,11 +11,18 @@ namespace RESTyard.AspNetCore.HypermediaUI;
 
 public static class HypermediaUiExtensions
 {
-    public static TAppBuilder UseHypermediaUI<TAppBuilder>(
-        this TAppBuilder builder,
+    /// <summary>
+    /// Host the Angular Application HypermediaUI in the given subpath (or root) by serving the contents from memory, and redirecting relevant alias routes to index.html.
+    /// </summary>
+    /// <param name="builder">The app builder</param>
+    /// <param name="subpath">The path under which to serve the HypermediaUI. Can contain slashes, leading or trailing slashes are normalized and can be omitted.</param>
+    /// <param name="config">The HypermediaUiConfig to serve with the HypermediaUI. Can also be configured as an IOption with the IServiceProvider, which this method will attempt to read if it is not passed explicitly.</param>
+    /// <typeparam name="TAppBuilder"></typeparam>
+    /// <returns>The app builder</returns>
+    public static IApplicationBuilder UseHypermediaUI(
+        this IApplicationBuilder builder,
         string subpath,
         HypermediaUiConfig? config = null)
-        where TAppBuilder : IApplicationBuilder
     {
         var files = ExtractAngularFilesFromArchive();
 
@@ -28,13 +35,12 @@ public static class HypermediaUiExtensions
             files,
             config);
         
-        builder.UseStaticFiles(new StaticFileOptions()
-        {
-            FileProvider = hypermediaFileProvider,
-            ContentTypeProvider = hypermediaFileProvider,
-        });
-        
-        return builder;
+        return builder.UseStaticFiles(
+            new StaticFileOptions()
+            {
+                FileProvider = hypermediaFileProvider,
+                ContentTypeProvider = hypermediaFileProvider,
+            });
     }
 
     private static IReadOnlyList<(string Name, string FullName, byte[] Content)> ExtractAngularFilesFromArchive()
