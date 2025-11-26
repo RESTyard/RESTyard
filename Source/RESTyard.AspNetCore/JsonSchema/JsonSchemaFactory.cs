1b128d9eed3f0f5676b8575d5ee21155c1341fdb
Mathias Reichardt <reichardt@data-cybernetics.com>
2025-11-26 01:06:55 +0100

    Fix DateOnly and TimeOnly. [Required] not working. - Tests fail because URi is now in $defs as reference and tests dont look there

--- Source/RESTyard.AspNetCore/JsonSchema/JsonSchemaFactory.cs
+++ Source/RESTyard.AspNetCore/JsonSchema/JsonSchemaFactory.cs
@@ -2,11 +2,39 @@
 using System.Text.Json;
 using Json.Schema;
 using Json.Schema.Generation;
+using Json.Schema.Generation.Generators;
+using Json.Schema.Generation.Intents;
+
 
 namespace RESTyard.AspNetCore.JsonSchema
 {
+    // DataAnnotations like [Required] not handled. 'required' keyword works
+    // required to support e.g. [Required] but this is an own class by the lib
+    // using Json.Schema.Generation.DataAnnotations;
+    // DataAnnotationsSupport.AddDataAnnotations();
     public static class JsonSchemaFactory
     {
+        static JsonSchemaFactory()
+        {
+            Config = new SchemaGeneratorConfiguration()
+            {
+                Generators = {
+                    new DateOnlyGenerator(),
+                    new TimeOnlyGenerator(),
+                    new UriSchemaGenerator()
+                },
+                
+            };
+            
+            // Config.Refiners.Add(new UriFormatRefiner());
+            AttributeHandler.AddHandler(new DisplayNameAttributeHandler());
+            AttributeHandler.AddHandler(new DescriptionAttributeHandler());
+            
+            
+        }
+
+        private static readonly SchemaGeneratorConfiguration Config;
+        
         public static object Generate(Type type)
         {
             var schema = GenerateSchema(type);
@@ -17,8 +45,72 @@ namespace RESTyard.AspNetCore.JsonSchema
         {
             return new JsonSchemaBuilder()
                 .Schema(MetaSchemas.Draft202012Id)
-                .FromType(type)
+                .FromType(type, Config)
                 .Build();
+        }
+    }
+    
+    // see https://github.com/json-everything/json-everything/issues/143
+    // we need to use the generic interface so it will be called
+    internal sealed class DisplayNameAttributeHandler : IAttributeHandler<System.ComponentModel.DisplayNameAttribute> {
+        public void AddConstraints(SchemaGenerationContextBase context, Attribute attribute)
+        {
+            var att = (System.ComponentModel.DisplayNameAttribute)attribute;
+             context.Intents.Insert(0, new TitleIntent(att.DisplayName));
+        }
+    }
+    internal sealed class DescriptionAttributeHandler : IAttributeHandler<System.ComponentModel.DescriptionAttribute> {
+        public void AddConstraints(SchemaGenerationContextBase context, Attribute attribute)
+        {
+            var att = (System.ComponentModel.DescriptionAttribute)attribute;
+            context.Intents.Insert(0, new TitleIntent(att.Description));
+        }
+    }
+    
+    public class DateOnlyGenerator : ISchemaGenerator
+    {
+        public bool Handles(Type type)
+        {
+            // Handle both DateOnly and DateOnly?
+            return type == typeof(DateOnly) || type == typeof(DateOnly?);
+        }
+
+        public void AddConstraints(SchemaGenerationContextBase context)
+        {
+            context.Intents.Add(new TypeIntent(SchemaValueType.String));
+            context.Intents.Add(new FormatIntent(Formats.Date)); 
+        }
+    }
+
+    public class TimeOnlyGenerator : ISchemaGenerator
+    {
+        public bool Handles(Type type)
+        {
+            return type == typeof(TimeOnly) || type == typeof(TimeOnly?);
+        }
+
+        public void AddConstraints(SchemaGenerationContextBase context)
+        {
+            context.Intents.Add(new TypeIntent(SchemaValueType.String));
+            context.Intents.Add(new FormatIntent(Formats.Time)); 
+        }
+    }
+    
+    public class UriSchemaGenerator : ISchemaGenerator
+    {
+        public bool Handles(Type type)
+        {
+            // This ensures it runs for Uri anywhere it appears
+            return type == typeof(Uri);
+        }
+    
+        public void AddConstraints(SchemaGenerationContextBase context)
+        {
+            // Force type: string
+            context.Intents.Add(new TypeIntent(SchemaValueType.String));
+        
+            // Force format: uri
+            context.Intents.Add(new FormatIntent(Formats.Uri));
         }
     }
 }
