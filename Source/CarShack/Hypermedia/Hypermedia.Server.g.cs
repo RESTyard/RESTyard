d611bd8ed4f5c8d1f07784eb5fe9d07e0de261aa
Mathias Reichardt <reichardt@data-cybernetics.com>
2025-12-04 13:02:48 +0100

    More undo

--- Source/CarShack/Hypermedia/Hypermedia.Server.g.cs
+++ Source/CarShack/Hypermedia/Hypermedia.Server.g.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using FunicularSwitch;
 using Microsoft.AspNetCore.Routing;
