a0c14a8ff5920c1319b3429ab0186ef8dd73db87
Mathias Reichardt <MathiasReichardt@users.noreply.github.com>
2025-12-04 13:22:41 +0100

    Modernize schema generation (#119)
    * Migrate schema generation to JsonSchema.Net to generate schemas in Draft 2020-12
    - migrate tests (still failing)
    
    * Fix DateOnly and TimeOnly. [Required] not working.
    - Tests fail because URi is now in $defs as reference and tests dont look there
    
    * Remove unnecessary generator since the original issue was ref schemas.
    Fix tests by resolving ref schemas
    
    * Make [Required] attributes to required keyword which is picked up by schema generator
    
    * Fix tests by resolving ref schemas
    
    * Make JSON schema factory a singleton
    - use JsonDocument as typed return. It still allows for some flexibility when creating alternative
      implementations because we do not return a type from Json.Schema lib.
    - add config entry to change it
    
    * Fix wrong intent
    
    * Return correct Content-Type for schemas
    
    * Undo edit in generated file
    
    * More undo
    
    ---------
    
    Co-authored-by: Mathias Reichardt <reichardt@data-cybernetics.com>

--- Source/RESTyard.AspNetCore/JsonSchema/JsonSchemaFactory.cs
+++ Source/RESTyard.AspNetCore/JsonSchema/JsonSchemaFactory.cs
@@ -1,38 +1,93 @@
 ﻿using System;
-using NJsonSchema.Generation;
 using System.Text.Json;
-using Newtonsoft.Json.Converters;
+using Json.Schema;
+using Json.Schema.Generation;
+using Json.Schema.Generation.Generators;
+using Json.Schema.Generation.Intents;
 
 namespace RESTyard.AspNetCore.JsonSchema
 {
-    public static class JsonSchemaFactory
+    // DataAnnotations like [Required] not handled. 'required' keyword works
+    // required to support e.g. [Required] but this is an own class by the lib
+    // using Json.Schema.Generation.DataAnnotations;
+    // DataAnnotationsSupport.AddDataAnnotations();
+    public class JsonSchemaFactory : IJsonSchemaFactory
     {
-        static readonly JsonSchemaGeneratorSettings JsonSchemaGeneratorSettings = new JsonSchemaGeneratorSettings
+        public JsonSchemaFactory()
         {
-            FlattenInheritanceHierarchy = true,
-            SerializerSettings = new()
+            config = new SchemaGeneratorConfiguration()
             {
-                Converters =
-                {
-                    new StringEnumConverter(),
+                Generators = {
+                    new DateOnlyGenerator(),
+                    new TimeOnlyGenerator(),
                 },
-            },
-        };
-
-        public static object Generate(Type type)
-
-        {
-            var schema = GenerateSchemaAsync(type);
-            var schemaData = schema.ToJson();
-
-            return JsonDocument.Parse(schemaData);
+            };
+            
+            AttributeHandler.AddHandler(new DisplayNameAttributeHandler());
+            AttributeHandler.AddHandler(new DescriptionAttributeHandler());
         }
 
-        public static NJsonSchema.JsonSchema GenerateSchemaAsync(Type type)
+        private readonly SchemaGeneratorConfiguration config;
+        
+        public JsonDocument Generate(Type type)
         {
-            var schema = NJsonSchema.JsonSchema.FromType(type, JsonSchemaGeneratorSettings);
+            var schema =new JsonSchemaBuilder()
+                .Schema(MetaSchemas.Draft202012Id)
+                .FromType(type, config)
+                .Build();
+            return JsonSerializer.SerializeToDocument(schema);
+        }
 
-            return schema;
+        public Json.Schema.JsonSchema GenerateSchema(Type type)
+        {
+            return Generate(type).RootElement.Deserialize<Json.Schema.JsonSchema>()
+                   ?? throw new Exception($"Can not create schema from presented {nameof(JsonDocument)}");
+        }
+    }
+
+    // see https://github.com/json-everything/json-everything/issues/143
+    // we need to use the generic interface so it will be called
+    internal sealed class DisplayNameAttributeHandler : IAttributeHandler<System.ComponentModel.DisplayNameAttribute> {
+        public void AddConstraints(SchemaGenerationContextBase context, Attribute attribute)
+        {
+            var att = (System.ComponentModel.DisplayNameAttribute)attribute;
+             context.Intents.Insert(0, new TitleIntent(att.DisplayName));
+        }
+    }
+    internal sealed class DescriptionAttributeHandler : IAttributeHandler<System.ComponentModel.DescriptionAttribute> {
+        public void AddConstraints(SchemaGenerationContextBase context, Attribute attribute)
+        {
+            var att = (System.ComponentModel.DescriptionAttribute)attribute;
+            context.Intents.Insert(0, new DescriptionIntent(att.Description));
+        }
+    }
+    
+    public class DateOnlyGenerator : ISchemaGenerator
+    {
+        public bool Handles(Type type)
+        {
+            // Handle both DateOnly and DateOnly?
+            return type == typeof(DateOnly) || type == typeof(DateOnly?);
+        }
+
+        public void AddConstraints(SchemaGenerationContextBase context)
+        {
+            context.Intents.Add(new TypeIntent(SchemaValueType.String));
+            context.Intents.Add(new FormatIntent(Formats.Date)); 
+        }
+    }
+
+    public class TimeOnlyGenerator : ISchemaGenerator
+    {
+        public bool Handles(Type type)
+        {
+            return type == typeof(TimeOnly) || type == typeof(TimeOnly?);
+        }
+
+        public void AddConstraints(SchemaGenerationContextBase context)
+        {
+            context.Intents.Add(new TypeIntent(SchemaValueType.String));
+            context.Intents.Add(new FormatIntent(Formats.Time)); 
         }
     }
 }
